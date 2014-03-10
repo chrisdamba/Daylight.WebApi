3781e1cbc7331e889b6033b93c09b1b76f171de7
Christopher Dambamuromo <chridam@gmail.com>
2014-03-10 23:01:28 +0000

    Error pages
    Adding error pages

---
+++ Daylight.WebApi.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace Daylight.WebApi.Mvc.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public ActionResult Access()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult HttpError()
+        {
+            return View();
+        }
+
+        public ActionResult Validation()
+        {
+            return View();
+        }
+    }
+}
