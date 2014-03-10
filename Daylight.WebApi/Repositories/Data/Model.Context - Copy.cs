154e3defb30e82c4f37fe455947daeedccfa78ce
Christopher Dambamuromo <chridam@gmail.com>
2014-03-10 19:42:31 +0000

    Updating EF changes
    Updating solution with EF changes

--- Daylight.WebApi/Repositories/Data/Model.Context - Copy.cs
+++ Daylight.WebApi/Repositories/Data/Model.Context - Copy.cs
@@ -13,25 +13,26 @@ namespace Daylight.WebApi.Repositories.Data
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     using Daylight.WebApi.Contracts.Entities;
-    
+
     public partial class DaylightDataContext : DbContext
     {
         public DaylightDataContext()
             : base("name=DaylightDataContext")
         {
+            Configuration.ProxyCreationEnabled = false;
         }
 
         public DaylightDataContext(string connectionString)
             : base(connectionString)
         {
-    		Configuration.ProxyCreationEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
         }
-    
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
         }
-    
+
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Condition> Conditions { get; set; }
         public DbSet<Medication> Medications { get; set; }
